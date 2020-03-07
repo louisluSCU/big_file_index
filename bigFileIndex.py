ac52155b3edf3a0287e6c0548c160c00a3028c5e
luye <luye19980902@163.com>
2020-03-07 15:28:40 +0800

    hash conflict in IndexBuilder with linked address

--- bigFileIndex.py
+++ bigFileIndex.py
@@ -7,13 +7,14 @@ INDEX_FILE = 'index.log'
 SLICE_SIZE = 2**30
 KEY_SIZE_S = 2
 VALUE_SIZE_S = 2
+# TODO: HASH_SIZE = 4
 HASH_SIZE = 1
 
 
 class IndexBuilder:
 
     def __init__(self):
-        pass
+        self.tail = 2**(HASH_SIZE*8)*6
 
     # Read 2GB data at a time
     def __read_file_slice(self):
@@ -33,10 +34,45 @@ class IndexBuilder:
             h.update(item[0])
             hash_v = int(h.hexdigest(), 16)
             print(hash_v)
-            # TODO: hash conflict
-            fp.seek(hash_v*5)
-            fp.write(item[1])
+            self.__write_index(hash_v, [item[1],], fp)
         fp.close()
+
+    def __write_index(self, offset, data, fp):
+        fp.seek(offset*6)
+        tuple = fp.read(6)
+
+        if tuple == b'':
+            if offset == self.tail:
+                self.tail += len(data)*6
+            for item in data:
+                print(item)
+                fp.write((0).to_bytes(1, byteorder='big') + item)
+        elif tuple == b'\x00\x00\x00\x00\x00\x00':
+            fp.seek(-6, 1)
+            for item in data:
+                print(item)
+                fp.write((0).to_bytes(1, byteorder='big') + item)
+        else:
+            fp.seek(-6, 1)
+            number_dup = int.from_bytes(fp.read(1), byteorder='big')
+
+            if number_dup == 255:
+                raise OverflowError
+
+            fp.seek(-1, 1)
+            fp.write((number_dup + 1).to_bytes(1, byteorder='big'))
+            next_node = fp.read(5)
+
+            # Real data tuple
+            if number_dup == 0:
+                fp.seek(-5, 1)
+                fp.write(self.tail.to_bytes(5, byteorder='big'))
+                data.append(next_node)
+                self.__write_index(self.tail, data, fp)
+
+            # Index tuple
+            else:
+                self.__write_index(next_node, data, fp)
 
     def build(self):
         counter = 0
@@ -53,7 +89,6 @@ class IndexBuilder:
 
                 # Calculate offset in file
                 index = (SLICE_SIZE * counter + fp.tell()).to_bytes(5, byteorder='big')
-                print(index)
                 kv_pairs.append((key, index))
                 value_size = fp.read(VALUE_SIZE_S)
                 # TODO: fp.read(int.from_bytes(value_size, byteorder='big'))
@@ -70,11 +105,14 @@ class DataReader:
         pass
 
     def get(self, key):
+        value = None
+
         if type(key) is not 'bytes':
             try:
                 key = key.encode('ascii')
             except:
                 print("Unsupported key type")
+                return value
 
         h = hashlib.blake2b(digest_size=HASH_SIZE)
         h.update(key)
@@ -82,27 +120,38 @@ class DataReader:
 
         # Search index
         fp = open(INDEX_FILE, 'rb')
-        fp.seek(hash_v*5)
-        offset = fp.read(5)
+        fp.seek(hash_v*6)
+        tuple = fp.read(6)
 
         # Key not exist
-        if offset == b'\x00\x00\x00\x00\x00':
-            return None
-        offset = int.from_bytes(offset, byteorder='big')
-        fp.close()
+        if tuple in (b'\x00\x00\x00\x00\x00\x00', b''):
+            pass
 
-        # Search file with offset
-        fp = open(INPUT_FILE, 'r')
-        fp.seek(offset)
-        value_size = int(fp.read(VALUE_SIZE_S))
-        value = fp.read(value_size)
-        fp.close()
+        else:
+            partial_key = tuple[0:1]
 
+            # No duplicate
+            if partial_key == (0).to_bytes(1, byteorder='big'):
+                offset = int.from_bytes(tuple[1:], byteorder='big')
+
+                fp = open(INPUT_FILE, 'r')
+                fp.seek(offset)
+                value_size = int(fp.read(VALUE_SIZE_S))
+                value = fp.read(value_size)
+                fp.close()
+            # TODO: Linear probe
+            else:
+                pass
+
+        fp.close()
         return value
 
 
+
+
+
 if __name__ == "__main__":
-    # fib = IndexBuilder()
-    # fib.build()
+    fib = IndexBuilder()
+    fib.build()
     fr = DataReader()
     print(fr.get('AAA'))
